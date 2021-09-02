1ba4ba4eff67816d953f86e16cff54b54e58f384
Alex Red Dev <garishnavi@gmail.com>
2021-09-02 15:44:47 +0300

    create easy structure project

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/navigationr.dart';
 
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      theme: ThemeData(),
+      home: Navigation(),
+    );
+  }
+}
